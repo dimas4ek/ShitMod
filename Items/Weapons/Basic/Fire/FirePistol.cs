6b62c4200e6db936e065850ba33fec638aae5493
SplitKrosGT <albertsadofficial@gmail.com>
2022-10-20 23:05:00 +0500

    add Fire Pistol

--- Items/Weapons/Basic/Fire/FirePistol.cs
+++ Items/Weapons/Basic/Fire/FirePistol.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
-using IL.Terraria.DataStructures;
-using Microsoft.Xna.Framework;
-using ShitMod.Projectiles.xuy;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using EntitySource_ItemUse_WithAmmo = Terraria.DataStructures.EntitySource_ItemUse_WithAmmo;
+using ShitMod.Projectiles;
+using ShitMod.Buffs;
+using ShitMod.Projectiles.xuy;
 
 namespace ShitMod.Items.Weapons.Basic.Fire
 {
@@ -13,7 +13,7 @@ namespace ShitMod.Items.Weapons.Basic.Fire
 {
     public override void SetStaticDefaults()
     {
-        Tooltip.SetDefault("'For Fury'");
+        Tooltip.SetDefault("'Fire'");
     }
     public int numShots = 1;
     public override void SetDefaults()
@@ -38,29 +38,33 @@ namespace ShitMod.Items.Weapons.Basic.Fire
         Item.autoReuse = false;
     }
 
-    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity,
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage,
+            ref float knockback)
+        {
+            if (type == ProjectileID.Bullet)
+            {
+                type = ModContent.ProjectileType<FireBullet>();
+            }
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity,
         int type,
         int damage, float knockback)
     {
         Vector2 offset = new Vector2(velocity.X * 5, velocity.Y * 5);
         position += offset;
 
-        if (type == ProjectileID.Bullet)
-        {
-            type = ModContent.ProjectileType<GelBulletProjectile>();
-
-        }
-
         return true;
     }
-
-    public override Vector2? HoldoutOffset()
+        public override void ModifyHitNPC(Player player, NPC target, ref int damage, ref float knockBack, ref bool crit)
+        {
+            target.AddBuff(ModContent.BuffType<BetterOnFire>(), 120);
+        }
+        public override Vector2? HoldoutOffset()
     {
         return new Vector2(-26, 4);
     }
-
-
-    public override void AddRecipes()
+        public override void AddRecipes()
     {
     }
 }
---
+++ Projectiles/FireBullet.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ShitMod.Buffs;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShitMod.Projectiles
+{
+    public class FireBullet : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Example Bullet"); // The English name of the projectile
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5; // The length of old position to be recorded
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0; // The recording mode
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 8;
+            Projectile.height = 8;
+            Projectile.aiStyle = 1;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.penetrate = 5;
+            Projectile.timeLeft = 600;
+            Projectile.alpha = 255;
+            Projectile.light = 0.5f;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 1; 
+
+            AIType = ProjectileID.Bullet;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.penetrate--;
+            if (Projectile.penetrate <= 0)
+            {
+                Projectile.Kill();
+            }
+            else
+            {
+                Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
+                SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+
+                if (Math.Abs(Projectile.velocity.X - oldVelocity.X) > float.Epsilon)
+                {
+                    Projectile.velocity.X = -oldVelocity.X;
+                }
+
+                if (Math.Abs(Projectile.velocity.Y - oldVelocity.Y) > float.Epsilon)
+                {
+                    Projectile.velocity.Y = -oldVelocity.Y;
+                }
+            }
+
+            return false;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(ModContent.BuffType<BetterOnFire>(), 120);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Main.instance.LoadProjectile(Projectile.type);
+            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+
+            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            for (int k = 0; k < Projectile.oldPos.Length; k++)
+            {
+                Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+            }
+
+            return true;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+        }
+    }
+}
