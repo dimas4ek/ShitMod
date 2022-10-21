71841095a44f6e2f47daa8ec227561d77777a43e
Dmitry Gulyaev <dimas4ek@gmail.com>
2022-10-21 21:42:26 +0500

    water scythe (BasicSword.cs)

--- Items/Weapons/xuy/BasicSword.cs
+++ Items/Weapons/xuy/BasicSword.cs
@@ -28,32 +28,32 @@ namespace ShitMod.Items.Weapons.xuy
             Item.rare = ItemRarityID.Blue;
             Item.UseSound = SoundID.Item1;
             Item.autoReuse = true;
-            Item.shoot = ModContent.ProjectileType<TestProjectile>();
+            //Item.shoot = ModContent.ProjectileType<TestProjectile>();
             Item.shootSpeed = 10f;
 
         }
 
-        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
-        {
-            target.AddBuff(BuffID.OnFire, 120);
-        }
+        //public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        //{
+        //    target.AddBuff(BuffID.OnFire, 120);
+        //}
 
-        public override void MeleeEffects(Player player, Rectangle hitbox)
-        {
-            var dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.Torch, 0f, 0f, 0, new Color(255, 255, 0), 2f);
-            Main.dust[dust].noGravity = true;
-            Main.dust[dust].velocity *= 0f;
-        }
+        //public override void MeleeEffects(Player player, Rectangle hitbox)
+        //{
+        //    var dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.Torch, 0f, 0f, 0, new Color(255, 255, 0), 2f);
+        //    Main.dust[dust].noGravity = true;
+        //    Main.dust[dust].velocity *= 0f;
+        //}
 
-        public override void AddRecipes()
-        {
-            var recipe = CreateRecipe();
-            recipe
-                .AddIngredient(ItemID.Diamond, 10)
-                .AddTile(TileID.LunarCraftingStation)
-                .Register();
-            //recipe.AddTile(TileID.WorkBenches);
-            //recipe.Register();
-        }
+        //public override void AddRecipes()
+        //{
+        //    var recipe = CreateRecipe();
+        //    recipe
+        //        .AddIngredient(ItemID.Diamond, 10)
+        //        .AddTile(TileID.LunarCraftingStation)
+        //        .Register();
+        //    //recipe.AddTile(TileID.WorkBenches);
+        //    //recipe.Register();
+        //}
     }
 }
