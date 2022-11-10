c72927e85b8bcb91417865afa89dbd5eb82f47bd
SplitKrosGT <albertsadofficial@gmail.com>
2022-11-10 20:36:29 +0500

    fix Kratos

--- Items/Weapons/Advanced/Kratos.cs
+++ Items/Weapons/Advanced/Kratos.cs
@@ -1,7 +1,9 @@
-﻿using ShitMod.Projectiles.Weapons;
+﻿using Microsoft.Xna.Framework;
+using ShitMod.Projectiles.Weapons;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace ShitMod.Items.Weapons.Advanced
@@ -10,36 +12,42 @@ namespace ShitMod.Items.Weapons.Advanced
     {
         public override void SetStaticDefaults()
         {
+            Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
             DisplayName.SetDefault("Kratos");
             Tooltip.SetDefault("'Kratos'");
+            Item.CloneDefaults(ItemID.WoodenBoomerang);
         }
 
         public override void SetDefaults()
         {
             Item.damage = 100;
-            Item.noMelee = true;
-            Item.width = 12;
-            Item.height = 12;
-            Item.useTime = 15;
-            Item.useAnimation = 15;
-            Item.useStyle = 1;
-            Item.knockBack = 6;
-            Item.rare = ItemRarityID.Blue;
-            Item.UseSound = SoundID.Item45;
-            Item.autoReuse = true;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 30;
+            Item.height = 30;
+            Item.useTime = 25;
+            Item.useAnimation = 25;
+            Item.noUseGraphic = true;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 3;
+            Item.value = 100000;
+            Item.rare = ItemRarityID.Pink;
             Item.shoot = ModContent.ProjectileType<KratosProjectile>();
             Item.shootSpeed = 15f;
-            Item.useTurn = true;
-            Item.maxStack = 1;
-            Item.consumable = false;
-            Item.noUseGraphic = true;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
         }
 
-        public override void AddRecipes()
+        public override bool AltFunctionUse(Player player)
         {
-            CreateRecipe()
-                .AddIngredient(ItemID.Wood, 10)
-                .Register();
+            return true;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (player.altFunctionUse == 2)
+                damage = (int)(damage * 0.75);
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+            return false;
         }
     }
 }
--- Projectiles/Weapons/KratosProjectile.cs
+++ Projectiles/Weapons/KratosProjectile.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
-using ShitMod.Buffs;
-using ShitMod.Dusts;
-using System;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -10,45 +9,136 @@ namespace ShitMod.Projectiles.Weapons
 {
     public class KratosProjectile : ModProjectile
     {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("KratosProjectile");
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 6;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+
         public override void SetDefaults()
         {
-            Projectile.width = 24;
-            Projectile.height = 24;
+            Projectile.DamageType = DamageClass.Melee;
             Projectile.friendly = true;
-            Projectile.aiStyle = 0;
-            Projectile.penetrate = 3;
-            Projectile.extraUpdates = 1;
-            Projectile.hostile = false;
-            Projectile.ignoreWater = true;
-            Projectile.tileCollide = true;
+            Projectile.light = 0.4f;
+
+            Projectile.width = 50;
+            Projectile.height = 50;
+            Projectile.penetrate = -1;
+            Projectile.aiStyle = -1;
         }
 
-        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        public override bool? CanDamage()
         {
-            target.AddBuff(ModContent.BuffType<BetterOnFire>(), 300);
+            return false;
         }
-        //я хрен знает как сделать зенит
+
+        public override bool PreAI()
+        {
+            if (Projectile.ai[0] == 1)
+            {
+                Projectile.ai[1]++;
+
+                //stay in place
+                Projectile.position = Projectile.oldPosition;
+                Projectile.velocity = Vector2.Zero;
+                Projectile.rotation += Projectile.direction * -0.4f;
+
+                if (Projectile.ai[1] > 15)
+                {
+                    Projectile.ai[0] = 2;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+
         public override void AI()
         {
-            Projectile.velocity.X -= 0.7f;
-            Projectile.velocity.Y += 0.1f;
-            if (Projectile.velocity.X < 0)
-                Projectile.spriteDirection = -1;
-            Projectile.rotation += Projectile.velocity.X / 64;
+            //вылет
+            if (Projectile.ai[0] == 0)
+            {
+                Projectile.ai[1]++;
 
+                if (Projectile.ai[1] > 30)
+                {
+                    Projectile.ai[0] = 1;
+                    Projectile.ai[1] = 0;
+                    Projectile.netUpdate = true;
+                }
+            }
+            //возвращение к игроку
+            else if (Projectile.ai[0] == 2)
+            {
+                Projectile.extraUpdates = 0;
+                Projectile.velocity = Vector2.Normalize(Main.player[Projectile.owner].Center - Projectile.Center) * 15;
 
-            Projectile.velocity.Y += Projectile.ai[0];
-            if (!Main.rand.NextBool(3)) return;
-            var dust = Dust.NewDust(Projectile.Center, 1, 1, ModContent.DustType<PoisonDust>(), 0f, 0f, 0, default(Color), 3.5f);
-            Main.dust[dust].scale = 1.5f;
+                //убейство, когда вернется к игроку
+                if (Projectile.Distance(Main.player[Projectile.owner].Center) <= 30)
+                    Projectile.Kill();
+            }
+
+            //вращение
+            Projectile.rotation += Projectile.direction * -0.4f;
+
+            //dust
+            int dustId = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y + 2f), Projectile.width, Projectile.height + 5, 60, Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 100, default(Color), 2f);
+            Main.dust[dustId].noGravity = true;
+
+            if (Projectile.ai[0] == 1)
+            {
+                Projectile.localAI[0] += 0.1f;
+                Projectile.position += Projectile.DirectionTo(Main.player[Projectile.owner].Center) * Projectile.localAI[0];
+
+                if (Projectile.Distance(Main.player[Projectile.owner].Center) <= Projectile.localAI[0])
+                    Projectile.Kill();
+            }
         }
 
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
-            Projectile.Kill();
+            if (Projectile.ai[0] == 0)
+            {
+                Projectile.ai[0] = 1;
+                Projectile.ai[1] = 0;
+            }
+            Projectile.tileCollide = false;
+
             return false;
         }
 
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+        {
+            //колизия
+            width = 30;
+            height = 30;
+            return true;
+        }
 
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture2D13 = TextureAssets.Projectile[Projectile.type].Value;
+            int num156 = TextureAssets.Projectile[Projectile.type].Value.Height / Main.projFrames[Projectile.type];
+            int y3 = num156 * Projectile.frame; 
+            Rectangle rectangle = new Rectangle(0, y3, texture2D13.Width, num156);
+            Vector2 origin2 = rectangle.Size() / 2f;
+
+            Color color26 = lightColor;
+            color26 = Projectile.GetAlpha(color26);
+
+            for (int i = 0; i < ProjectileID.Sets.TrailCacheLength[Projectile.type]; i++)
+            {
+                Color color27 = color26;
+                color27 *= (float)(ProjectileID.Sets.TrailCacheLength[Projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[Projectile.type];
+                Vector2 value4 = Projectile.oldPos[i];
+                float num165 = Projectile.oldRot[i];
+                Main.EntitySpriteDraw(texture2D13, value4 + Projectile.Size / 2f - Main.screenPosition + new Vector2(0, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, Projectile.scale, SpriteEffects.None, 0);
+            }
+
+            Main.EntitySpriteDraw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Projectile.GetAlpha(lightColor), Projectile.rotation, origin2, Projectile.scale, SpriteEffects.None, 0);
+            return false;
+        }
     }
 }
